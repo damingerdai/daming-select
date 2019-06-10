6b0d3c86655a723151c692784376a84d918d5e07
Arthur Ming <guobin.ming@pwc.com>
2019-06-10 22:54:40 +0800

    docs: 修改demo页面，测试value属性的效果

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 const OPTIONS = [
   {
@@ -9,7 +10,7 @@ const OPTIONS = [
     value: 'xiao2',
     name: 'xiao2'
   }
-]
+];
 
 @Component({
   selector: 'app-root',
@@ -20,4 +21,28 @@ export class AppComponent {
   title = 'daming-select';
 
   options = OPTIONS;
+
+  form: FormGroup;
+
+  selectedValue: string;
+
+  get defaultValue() {
+    return this.form.get('defaultValue');
+  }
+
+  get formValid() {
+    return this.form.valid;
+  }
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.form = fb.group({
+      defaultValue: ['', [Validators.required]]
+    });
+  }
+
+  submit() {
+    this.selectedValue = this.defaultValue.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { SelectModule } from 'select';
 
 import { AppComponent } from './app.component';
-import { SelectModule } from 'select';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -12,7 +18,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     SelectModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
