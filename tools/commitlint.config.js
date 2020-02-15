88d73e9e98adb9776c14ed7175ee918ad2d95023
Arthur Ming <guobin.ming@pwc.com>
2020-02-15 23:45:18 +0800

    ci(commitlint): more commitlint config

--- tools/commitlint.config.js
+++ tools/commitlint.config.js
@@ -7,7 +7,8 @@ module.exports = {
 					"feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"
 			]],
 			'subject-full-stop': [0, 'never'],
-			'subject-case': [0, 'never']
-
+      'subject-case': [0, 'never'],
+      'header-max-length': [2, 'always', '108'],
+      'scope-case': [0, 'never']
 	}
 };
