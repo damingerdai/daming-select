35408e998b068ae334fb9abdcfd5088d3d850294
Arthur Ming <guobin.ming@pwc.com>
2020-04-27 10:32:33 +0800

    v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.2](https://github.com/damingerdai/daming-select/compare/v0.0.1...v0.0.2) (2020-04-27)
+
+
+
 ## 0.0.1 (2019-10-04)
 
 
--- tools/copy-changelog.js
+++ tools/copy-changelog.js
@@ -2,6 +2,6 @@ const chalk = require('chalk');
 const shell = require('shelljs');
 const log = console.log;
 
-if (shell.cp('-R', 'CHANGELOG.md', 'dist/select').code !== 0) {
+if (shell.cp('-R', 'CHANGELOG.md', 'dist/@daminggerdai/select').code !== 0) {
   log(chalk.red('fail to copy CHANGELOG.md to package folder'));
 }
