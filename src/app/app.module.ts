d1ae794d510ef46f6c1bc1c093bbc3d2c9cfc198
Arthur Ming <guobin.ming@pwc.com>
2020-07-01 11:56:23 +0800

    fix: add support new library path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { SelectModule } from 'select';
+import { SelectModule } from '@damingerdai/select';
 
 import { AppComponent } from './app.component';
 import { SharedMaterialModule } from './shared.material.module';
